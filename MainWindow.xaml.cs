7de03edaa8b6166effc0025cc69cdda1e57282eb
KittenKiller29 <azamatshsh123@gmail.com>
2025-03-30 20:26:43 +0500

    Добавлена логика работы ui

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Globalization;
+using System.Runtime;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +27,158 @@ namespace AutoTradeLauncher
         public MainWindow()
         {
             InitializeComponent();
+            LotSizeTB.PreviewTextInput += NumericTextBox_PreviewTextInput;
+            TPTB.PreviewTextInput += IntTextBox_PreviewTextInput;
+            SLTB.PreviewTextInput += IntTextBox_PreviewTextInput;
+            CountTB.PreviewTextInput += IntTextBox_PreviewTextInput;
+            DropTB.PreviewTextInput += IntTextBox_PreviewTextInput;
+
+            LotSizeTB.Text = Properties.Settings.Default.LotSize;
+            TPTB.Text = Properties.Settings.Default.TP;
+            SLTB.Text = Properties.Settings.Default.SL;
+            CountTB.Text = Properties.Settings.Default.Count;
+            DropTB.Text = Properties.Settings.Default.Drop;
+            CreateDefaultFile();
+        }
+
+        private void CreateDefaultFile()
+        {
+            try
+            {
+                // Получаем путь к папке с exe-файлом
+                string exePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string filePath = System.IO.Path.Combine(exePath, "data.txt");
+                if (File.Exists(filePath)) return;
+
+                // 5 значений для записи
+                string[] values = {
+                    "0.01",
+                    "48",
+                    "24",
+                    "1",
+                    "3"
+                };
+
+                // Записываем значения в файл
+                File.WriteAllLines(filePath, values);
+
+            }
+            catch (Exception ex)
+            {
+                //ConsoleLog.AppendText($"Ошибка при создании файла: {ex.Message}");
+            }
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = ValidateDatas();
+            if (ValidateDatas() != "")
+            {
+                MessageBox.Show(msg, "Неправильно заданы данные", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            //Валидацию прошли, сохраняем данные
+            Properties.Settings.Default.LotSize = LotSizeTB.Text;
+            Properties.Settings.Default.TP = TPTB.Text;
+            Properties.Settings.Default.SL = SLTB.Text;
+            Properties.Settings.Default.Count = CountTB.Text;
+            Properties.Settings.Default.Drop = DropTB.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private string ValidateDatas()
+        {
+            string msg = "";
+            if(!double.TryParse(LotSizeTB.Text, NumberStyles.AllowDecimalPoint,
+                CultureInfo.InvariantCulture,
+                out _))
+            {
+                msg = "Значение объем лота должно быть дробным.";
+            }
+            else if (!int.TryParse(TPTB.Text, out _))
+            {
+                msg = "Значение TP должно быть целым числом.";
+            }
+            else if (!int.TryParse(SLTB.Text, out _))
+            {
+                msg = "Значение SL должно быть целым числом.";
+            }
+            else if (!int.TryParse(CountTB.Text, out _))
+            {
+                msg = "Значение максимального числа сделок должно быть целым числом.";
+            }
+            else if (!int.TryParse(DropTB.Text, out _))
+            {
+                msg = "Значение объем неудачных сделок должно быть целым числом.";
+            }
+
+            return msg;
+        }
+
+        private void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            string newText = textBox.Text.Insert(textBox.CaretIndex, e.Text);
+
+            // Проверяем, что введённый текст — число с одной точкой/запятой
+            bool isNumeric = double.TryParse(newText,
+                NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign,
+                CultureInfo.InvariantCulture,
+                out _);
+
+            // Разрешаем только цифры и точку для дабла
+            bool isAllowedChar = e.Text.All(c => char.IsDigit(c) || c == '.');
+
+            e.Handled = !isNumeric || !isAllowedChar;
+        }
+
+        private void IntTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            string newText = textBox.Text.Insert(textBox.CaretIndex, e.Text);
+
+            // Проверяем, что введённый текст — число
+            bool isNumeric = int.TryParse(newText, out _);
+
+            // Разрешаем только цифры и точку для дабла
+            bool isAllowedChar = e.Text.All(c => char.IsDigit(c));
+
+            e.Handled = !isNumeric || !isAllowedChar;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.LotSize = LotSizeTB.Text;
+            Properties.Settings.Default.TP = TPTB.Text;
+            Properties.Settings.Default.SL = SLTB.Text;
+            Properties.Settings.Default.Count = CountTB.Text;
+            Properties.Settings.Default.Drop = DropTB.Text;
+            Properties.Settings.Default.Save();
+            base.OnClosed(e);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateDefaultFile();
+            string filename = "data.txt";
+            string exePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string filePath = System.IO.Path.Combine(exePath, filename);
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length != 5)
+            {
+                return;
+            }
+            LotSizeTB.Text = lines[0];
+            TPTB.Text = lines[1];
+            SLTB.Text = lines[2];
+            CountTB.Text = lines[3];
+            DropTB.Text = lines[4];
         }
     }
 }
